013602908be78796478f24e1553f9027dc49e100
malonecj <malonecj@gmail.com>
2015-01-13 11:39:18 +0000

    solution tidy up

--- standalone/gulp/tasks/watch.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('watch', ['setWatch', 'browserSync'], function() {
-	gulp.watch('themes/**', ['styles']);
-	gulp.watch('lib/**', ['browserify']);
-	// Note: The browserify task handles js recompiling with watchify
-});
--- standalone/index.js
+++
@@ -1,9 +0,0 @@
-;(function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define(['react'], factory);
-  } else {
-    root.ReactAutocomplete = factory(root.React);
-  }
-})(window, function(React) {
-  return require('../lib/index.js');
-});
